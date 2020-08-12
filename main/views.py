a314b93952b06aebc11fc712d3a012305d5c8a6e
alex <aleks.98.x.28@gmail.com>
2020-08-12 10:05:14 +0600

    Add posts list-view

--- main/views.py
+++ main/views.py
@@ -1,8 +1,26 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Post
+from .serializers import PostSerializer
+
 
 @api_view()
-def hello_world(request):
-    return Response('Hello, world!')
+def posts_list(request):
+    queryset = Post.objects.all()
+    serializer = PostSerializer(queryset, many=True, context={'request': request})
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+
+
+
+
+# TODO: CBV
+# TODO: CRUD
+# TODO: Viewsets
+# TODO: Pagination
+# TODO: Search
+# TODO: Filtering
+# TODO: Permission
+# TODO: Get author from request
