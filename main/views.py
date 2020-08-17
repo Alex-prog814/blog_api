5c19621cd1718e7d2e909d1523dd538c941b4f70
alex <aleks.98.x.28@gmail.com>
2020-08-17 14:07:35 +0600

    Add search

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Q
 
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -8,58 +11,98 @@ from .permissions import IsPostAuthor
 from .serializers import PostSerializer, CategorySerializer
 
 
-class CategoryListView(generics.ListAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+# class CategoryListView(generics.ListAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+#
+# class CategoryDetailsView(generics.RetrieveAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+#     lookup_field = 'slug'
+#
+#
+# class PostsListView(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class PostDetailsView(generics.RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
 
-class CategoryDetailsView(generics.RetrieveAPIView):
+# class CreatePostView(generics.CreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#     def create(self, request, *args, **kwargs):
+#         data = request.data
+#         serializer = PostSerializer(data=data, context={'request': request})
+#         serializer.is_valid(raise_exception=True)
+#         post = serializer.save()
+#         # post.author = request.user
+#         # post.save()
+#         serializer = PostSerializer(instance=post)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# class UpdatePostView(generics.UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [IsPostAuthor, ]
+#
+#
+# class DeletePostView(generics.DestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     permission_classes = [IsPostAuthor, ]
+
+
+class CategoriesViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'slug'
 
 
-class PostsListView(generics.ListAPIView):
+class MyCustomPagination(PageNumberPagination):
+    page_size = 2
+
+
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsPostAuthor]
+    pagination_class = MyCustomPagination
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def get_permissions(self):
+        if self.action in ['create', 'own']:
+            permissions = [IsAuthenticated, ]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            permissions = [IsAuthenticated, IsPostAuthor]
+        else:
+            permissions = []
+        return[permission() for permission in permissions]
+
+    @action(detail=False, methods=['get'])
+    def own(self, request, pk=None):
+        queryset = self.get_queryset()
+        queryset = queryset.filter(author=request.user)
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'])
+    def search(self, request, pk=None):
+        q = request.query_params.get('q')
+        queryset = self.get_queryset()
+        queryset = queryset.filter(Q(title__icontains=q) | Q(text__icontains=q) | Q(category__title__icontains=q))
+        serializer = PostSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class PostDetailsView(generics.RetrieveAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class CreatePostView(generics.CreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-
-    def create(self, request, *args, **kwargs):
-        data = request.data
-        serializer = PostSerializer(data=data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        post = serializer.save()
-        # post.author = request.user
-        # post.save()
-        serializer = PostSerializer(instance=post)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class UpdatePostView(generics.UpdateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsPostAuthor, ]
-
-
-class DeletePostView(generics.DestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [IsPostAuthor, ]
-
-
-
-
-# TODO: Viewsets
-# TODO: Pagination
 # TODO: Search(F(), Q())
 # TODO: Filtering
